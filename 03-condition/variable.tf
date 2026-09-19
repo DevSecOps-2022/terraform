2128032152cf06a4099a5cfe0d62fd63a1efa56c
mahan2526 <mamatha.m2526@gmail.com>
2026-09-20 00:27:00 +0530

    varibles

---
+++ 03-condition/variable.tf
@@ -0,0 +1,47 @@
+variable "environment" {
+  type    = string
+  default = "prod"
+
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "Environment must be either dev, staging, or prod."
+  }
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+
+  validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium", "t3.large"], var.instance_type)
+    error_message = "Instance type must be either t3.micro or t3.small."
+  }
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Name        = "ec2-demo"
+    Project     = "Roboshop"
+    Environment = "Dev"
+  }
+}
+
+variable "sg_name" {
+  type    = string
+  default = "terraform-sg"
+}
+
+variable "sg_tags" {
+  type = map(string)
+  default = {
+    Name        = "terraform-sg"
+    Project     = "Roboshop"
+    Environment = "Dev"
+  }
+}
